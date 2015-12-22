d952017da3e8eabbdf3076f18de5d0830966baa9
Anz <leejong32@naver.com>
2015-12-22 18:31:17 +0900

    button event

--- Catdog Engine/UI/Button.cs
+++ Catdog Engine/UI/Button.cs
@@ -3,15 +3,87 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Input;
 
 namespace CatdogEngine.UI {
+
+	/////////////////////////////////////////////////////////////////
+	// 이벤트 콜백을 위한 대리자
+	/////////////////////////////////////////////////////////////////
+	public delegate void BUTTON__MOUSE_IN();
+	public delegate void BUTTON__MOUSE_OUT();
+	public delegate void BUTTON__LEFT_MOUSE_DOWN(int x, int y);
+	public delegate void BUTTON__LEFT_MOUSE_UP(int x, int y);
+
+	/// <summary>
+	/// 버튼 UI
+	/// </summary>
 	public class Button : IStencil {
-		public void Draw(GameTime gameTime) {
-			
-		}
+		private Rectangle _region;							// 버튼의 영역
+		private bool _mouseHover;                           // 커서가 영역 안에 있는가
+		private bool _leftMousePressed;						// 마우스 왼쪽 버튼이 눌려있는가
+
+		private BUTTON__MOUSE_IN _onMouseIn;
+		private BUTTON__MOUSE_OUT _onMouseOut;
+		private BUTTON__LEFT_MOUSE_DOWN _onLeftMouseDown;
+		private BUTTON__LEFT_MOUSE_UP _onLeftMouseUp;
+
+		#region Properties
+		public BUTTON__MOUSE_IN OnMouseIn { set { _onMouseIn = value; } }
+		public BUTTON__MOUSE_OUT OnMouseOut { set { _onMouseOut = value; } }
+		public BUTTON__LEFT_MOUSE_DOWN OnLeftMouseDown { set { _onLeftMouseDown = value; } }
+		public BUTTON__LEFT_MOUSE_UP OnLeftMouseUp { set { _onLeftMouseUp = value; } }
+		#endregion
 
 		public void Update(GameTime gameTime) {
-			
+			// 현재 마우스 State를 본다.
+			MouseState mouseState = Mouse.GetState();
+
+			// 커서의 위치를 읽는다.
+			int mousePositionX = mouseState.X;
+			int mousePositionY = mouseState.Y;
+
+			if(_mouseHover) {
+				if(!_region.Contains(mousePositionX, mousePositionY)) {
+					// 현재 커서는 영역 밖에 있다.
+					_mouseHover = false;
+
+					// BUTTON__MOUSE_OUT 이벤트 발생.
+					if(_onMouseOut != null) _onMouseOut();
+				}
+			}
+			else {
+				if (_region.Contains(mousePositionX, mousePositionY)) {
+					// 현재 커서는 영역 안에 있다.
+					_mouseHover = true;
+
+					// BUTTON__MOUSE_IN 이벤트 발생.
+					if(_onMouseIn != null) _onMouseIn();
+				}
+			}
+
+			if(mouseState.LeftButton == ButtonState.Pressed) {
+				if(!_leftMousePressed) {
+					// 현재 왼쪽 마우스는 클릭 된 상태다.
+					_leftMousePressed = true;
+
+					// BUTTON__LEFT_MOUSE_DOWN 이벤트 발생
+					if(_onLeftMouseDown != null) _onLeftMouseDown(mousePositionX, mousePositionY);
+				}
+			}
+			else if(mouseState.LeftButton == ButtonState.Released) {
+				if(_leftMousePressed) {
+					// 현재 왼쪽 마우스는 클릭 되지 않은 상태다.
+					_leftMousePressed = false;
+
+					// BUTTON__LEFT_MOUSE_UP 이벤트 발생
+					if(_onLeftMouseUp != null) _onLeftMouseUp(mousePositionX, mousePositionY);
+				}
+			}
+		}
+
+		public void Draw(GameTime gameTime) {
+
 		}
 	}
 }
